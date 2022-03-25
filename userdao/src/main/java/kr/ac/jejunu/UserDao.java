b44f9a5dd31b4682eeb2cd69d09a027379f1891f
hkhh972123 <hkhh972123@gmail.com>
2022-03-25 19:53:44 +0900

    dependency injection

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -3,16 +3,14 @@ package kr.ac.jejunu;
 import java.sql.*;
 
 public class UserDao {
+    private final ConnectionMaker connectionMaker;
 
-    public static User findById(Integer id) throws ClassNotFoundException, SQLException {
-        // 드라이버 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        // 커넥션
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/jeju",
-                "jeju",
-                "d0dduck10"
-        );
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public User findById(Integer id) throws ClassNotFoundException, SQLException {
+        Connection connection = connectionMaker.getConnection();
         // sql 작성
         PreparedStatement preparedStatement =
                 connection.prepareStatement(
@@ -33,5 +31,29 @@ public class UserDao {
         connection.close();
         // User 리턴
         return user;
+    }
+
+    public void insert(User user) throws SQLException, ClassNotFoundException {
+        // 드라이버 로딩
+        Connection connection = connectionMaker.getConnection();
+        // sql 작성
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(
+                        "insert into userinfo(name, password) values (?, ?)"
+                        ,Statement.RETURN_GENERATED_KEYS
+                );
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        // sql 실행
+        preparedStatement.executeUpdate();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        // User에 데이터 매핑
+        user.setId(resultSet.getInt(1));
+        // 자원 해지
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        // User 리턴
     }
 }
